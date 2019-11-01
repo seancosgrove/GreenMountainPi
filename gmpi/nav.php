e39e9d46d73d951041415f2a1a1c087a50fe4ca1
emerywollerscheid <emerywollerscheid@gmail.com>
2019-11-01 15:04:26 -0400

    Added network to nav

--- gmpi/nav.php
+++ gmpi/nav.php
@@ -10,6 +10,14 @@
         print '<ahref="index.php">Home</a>';
         print '</li>';
 
+	print'<li class="';
+        if ($path_parts['filename'] == "network") {
+            print ' activePage ';
+        }
+        print '">';
+        print '<ahref="network.php">Nmap Capture</a>';
+        print '</li>';
+
        ?>
     </ol>
 </nav>
