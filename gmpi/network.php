31e8ad40a7a635aca5a07c592fb381d126e8711f
seancosgrove <Sean.M.Cosgrove@uvm.edu>
2019-11-01 15:16:34 -0400

    -M "Fixed php syntax"

--- gmpi/network.php
+++ gmpi/network.php
@@ -1,10 +1,14 @@
-<?
+<?php
 include "top.php";
+?>
 
+<?php
 $nmapFile = fopen("/home/pi/Desktop/final-project/network/nmapcapture.txt","r") or die("Unable to open file.");
 echo fread($nmapFile,filesize("/home/pi/Desktop/final-project/network/nmapcapture.txt","r"));
 fclose($nmapFile);
+?>
 
+<?php
 include "bot.php";
 ?>
 </body>
