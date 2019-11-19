0a821fa9aa346d59d6207475e772185fc48bd13c
emerywollerscheid <emerywollerscheid@gmail.com>
2019-11-19 12:03:42 -0500

    Created webpage for camera and added to nav

--- gmpi/nav.php
+++ gmpi/nav.php
@@ -18,6 +18,14 @@
         print '<a href="network.php">Network</a>';
         print '</li>';
 
+	print'<li class="';
+	if ($path_parts['filename'] == "surveilance") {
+		print ' activePage ';
+	}
+	print '">';
+	print '<a href="surveilance.php">Surveilance</a>';
+	print '</li>';
+
        ?>
     </ol>
 </nav>
